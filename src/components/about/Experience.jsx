6957cd40c14bd015c1121fc1f64085aa6af45445
Theodor Risager <theodor349@gmail.com>
2024-06-28 13:01:45 +0200

    Experiance

--- src/components/about/Experience.jsx
+++ src/components/about/Experience.jsx
@@ -11,19 +11,19 @@ const experienceContent = [
     year: "2021 - 2024",
     position: "Teaching Assitant",
     compnayName: "Aalborg University",
-    details: `Used my explaining skill to help newer students understand and apply course materials in Software Development and Syntax and Semantics`,
+    details: `Leveraged my strong communication skills, including clear explanations, active listening, and patience, to effectively aid over 200 students in grasping complex concepts within two courses: Systems Development and Syntax & Semantics`,
   },
   {
     year: "2021 - 2024",
     position: "Student RPA Developer",
     compnayName: "Aalborg University",
-    details: `Initialy I automated manual processes using UiPath and later I headed the development our our in house RPA management gateway`,
+    details: `As the first developer hired to develop RPA solutions using UiPath, I helped automate multiple processes, resulting in significant time savings and improved efficiency. Leveraging my newfound UiPath expertise, I then spearheaded the development of our in-house RPA management gateway. This solution facilitated comprehensive tracking of all UiPath executions, including logging, statistics, and asset management, allowing for better resource allocation and performance optimization`,
   },
   {
     year: "2020 - 2022",
     position: "Data Analysist",
     compnayName: "Epoka",
-    details: `Applyied my developer skills to automate their migration to their new ERP system`,
+    details: `By applying my programming expertise, I designed and implemented a automated solution that streamlined the migration process for their new ERP system.  This resulted in a dramatic improvement in efficiency, reducing the original one-week migration timeline to just two days`,
   },
 ];
 
