bfa1e19d0641e1afe95c96a6ec7fb05d4f336ff9
theodor349 <theodor349@gmail.com>
2026-09-22 11:43:47 +0200

    Rekura

--- components/portfolio.tsx
+++ components/portfolio.tsx
@@ -23,6 +23,12 @@ const screenshotsByProject: Record<
     { src: "/portfolio/WR02.png", width: 2048, height: 1280 },
     { src: "/portfolio/WR03.png", width: 2048, height: 1280 },
   ],
+  "Rekura": [
+    { src: "/portfolio/Rekura01.png", width: 2048, height: 1280 },
+    { src: "/portfolio/Rekura02.png", width: 2048, height: 1280 },
+    { src: "/portfolio/Rekura03.png", width: 2048, height: 1280 },
+    { src: "/portfolio/Rekura04.png", width: 2048, height: 1280 },
+  ],
   Folkbook: [
     { src: "/portfolio/Folkbook01.png", width: 744, height: 1472 },
     { src: "/portfolio/Folkbook02.png", width: 744, height: 1472 },
